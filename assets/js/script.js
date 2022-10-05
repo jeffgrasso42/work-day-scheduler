9b8aea128976418dce9e9029133d1a0adc4a384e
JAG <jagrasso42@gmail.com>
2022-10-05 17:07:52 -0400

    stored hourKey and textArea content in local storage, added .val() to textArea element to retrieve content from local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,12 +7,12 @@
 for (var i = 9; i < 18; i++) {
   var row = $('<div>').addClass('row time-block').attr('id', i);
   var hour = $('<div>').addClass('col-2 hour').text(i);
-  var textArea = $('<textarea>').addClass('col-8 description');
+  var textArea = $('<textarea>').addClass('col-8 description').val(localStorage.getItem(i)); // i = hourKey
   var saveBtn = $('<button>').attr('id', i).addClass('col-2 saveBtn').click(function () {
     var hourKey = $(this).attr('id');
 
     var activity = $(this).siblings('.description').val();
-    console.log(activity);
+    localStorage.setItem(hourKey, activity);
   })
   var icon = $('<i>').addClass('fas fa-save');
 
