289a21dcb30c6dc02fafd436dca358eb4bebf25f
JAG <jagrasso42@gmail.com>
2022-10-05 18:05:52 -0400

    new comments and minor refacoring for assets/js/script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,63 @@
-// create grid where 1 hour = 1 row
-// create inputs for each hour
-// get activity input from user
-// add button for each hour
-// add click event to save activity to local storage
+// DEPENDENCIES
+// https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js
 
-var currentDate = new Date().getHours();
+// ASSIGNMENT CODE
+var currentHour = new Date().getHours();
 
-for (var i = 9; i < 18; i++) {
-  var currentHour = new Date().getHours();
-  var colorKey = "";
-  if (currentHour > i) {
-    colorKey = "past";
-  } else if (currentHour === i) {
-    colorKey = "present";
-  } else {
-    colorKey = "future";
+// populates container div with schedule
+function init() {
+  // creates rows for 9AM-5PM
+  for (var i = 9; i < 18; i++) {
+    var colorKey = "";
+    // check if the current hour is after the hour on the scheduler
+    if (currentHour > i) {
+      colorKey = "past";
+      // check if the current hour is the same as on the scheduler
+    } else if (currentHour === i) { 
+      colorKey = "present";
+    } else { // hour is before hour on the schedule
+      colorKey = "future";
+    }
+  
+    var hourDisplay = "";
+  
+    // check if the hour on the row is before noon
+    if (i < 12) {
+      hourDisplay = i + "AM";
+    } else if (i === 12) { // check if the hour is noon
+      hourDisplay = i + "PM";
+    } else {
+      hourDisplay = i - 12 + "PM"; // convert hour to 12 hour time
+    }
+    
+    // create div element to contain an hour row 
+    var row = $("<div>").addClass("row time-block");
+    // create div element for hour
+    var hour = $("<div>").addClass("col-2 hour").text(hourDisplay);
+    // create textarea element to get and store user input
+    var textArea = $("<textarea>")
+      .addClass("col-8 description " + colorKey)
+      // i matches the hourKey stored in local storage
+      .val(localStorage.getItem(i)); 
+    // create button element with click event
+    var saveBtn = $("<button>")
+      .attr("id", i)
+      .addClass("col-2 saveBtn")
+      .click(function () {
+        // get the hour of the row from the button's id
+        var hourKey = $(this).attr("id");
+        // get user input from the textarea
+        var activity = $(this).siblings(".description").val();
+        // preserve the hourKey and input
+        localStorage.setItem(hourKey, activity);
+      });
+    // floppy-disc icon for button element
+    var icon = $("<i>").addClass("fas fa-save");
+  
+    // add created elements and their content to container element
+    $(".container").append(row.append(hour, textArea, saveBtn.append(icon)));
   }
-
-  var hourDisplay = "";
-
-  if (i < 12) {
-    hourDisplay = i + "AM";
-  } else if (i === 12) {
-    hourDisplay = i + "PM";
-  } else {
-    hourDisplay = i - 12 + "PM";
-  }
-
-  var row = $("<div>").addClass("row time-block").attr("id", i);
-  var hour = $("<div>").addClass("col-2 hour").text(hourDisplay);
-  var textArea = $("<textarea>")
-    .addClass("col-8 description " + colorKey)
-    .val(localStorage.getItem(i)); // i = hourKey
-  var saveBtn = $("<button>")
-    .attr("id", i)
-    .addClass("col-2 saveBtn")
-    .click(function () {
-      var hourKey = $(this).attr("id");
-
-      var activity = $(this).siblings(".description").val();
-      localStorage.setItem(hourKey, activity);
-    });
-  var icon = $("<i>").addClass("fas fa-save");
-
-  $(".container").append(row.append(hour, textArea, saveBtn.append(icon)));
 }
+
+// INITIALIZATION
+init();
