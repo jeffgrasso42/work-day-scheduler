93d098b426b7280741b005521fb37be3eb3a3e22
JAG <jagrasso42@gmail.com>
2022-10-05 17:03:18 -0400

    added click() function to saveBtn element in assets/js/script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,12 @@ for (var i = 9; i < 18; i++) {
   var row = $('<div>').addClass('row time-block').attr('id', i);
   var hour = $('<div>').addClass('col-2 hour').text(i);
   var textArea = $('<textarea>').addClass('col-8 description');
-  var saveBtn = $('<button>').addClass('col-2 saveBtn')
+  var saveBtn = $('<button>').attr('id', i).addClass('col-2 saveBtn').click(function () {
+    var hourKey = $(this).attr('id');
+
+    var activity = $(this).siblings('.description').val();
+    console.log(activity);
+  })
   var icon = $('<i>').addClass('fas fa-save');
 
   $('.container').append(
