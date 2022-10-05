5ff4b5d66b2b943cee2f63d74ed24631585b132c
JAG <jagrasso42@gmail.com>
2022-10-05 16:56:39 -0400

    created rows of hours with for loop in assets/js/script.js

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+// create grid where 1 hour = 1 row
+// create inputs for each hour
+// get activity input from user
+// add button for each hour
+// add click event to save activity to local storage
+
+for (var i = 9; i < 18; i++) {
+  var row = $('<div>').addClass('row time-block').attr('id', i);
+  var hour = $('<div>').addClass('col-2 hour').text(i);
+  var textArea = $('<textarea>').addClass('col-8 description');
+  var saveBtn = $('<button>').addClass('col-2 saveBtn')
+  var icon = $('<i>').addClass('fas fa-save');
+
+  $('.container').append(
+    row.append(hour, textArea, saveBtn.append(icon)));
+}
