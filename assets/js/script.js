49fca54c14a3ce61952f96ffd96282c22ad2791c
JAG <jagrasso42@gmail.com>
2022-10-05 17:20:54 -0400

    added variables for  JavaScript Date() object to color code textarea elements based on whether they were past, present, or future

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,22 @@
 // add button for each hour
 // add click event to save activity to local storage
 
+var currentDate = new Date().getHours();
+console.log(currentDate);
+
 for (var i = 9; i < 18; i++) {
+  var currentHour = new Date().getHours();
+  var colorKey = '';
+  if (currentHour > i) {
+    colorKey = 'past';
+  } else if (currentHour === i) {
+    colorKey = 'present'
+  } else {
+    colorKey = 'future';
+  }
   var row = $('<div>').addClass('row time-block').attr('id', i);
   var hour = $('<div>').addClass('col-2 hour').text(i);
-  var textArea = $('<textarea>').addClass('col-8 description').val(localStorage.getItem(i)); // i = hourKey
+  var textArea = $('<textarea>').addClass('col-8 description ' + colorKey).val(localStorage.getItem(i)); // i = hourKey
   var saveBtn = $('<button>').attr('id', i).addClass('col-2 saveBtn').click(function () {
     var hourKey = $(this).attr('id');
 
