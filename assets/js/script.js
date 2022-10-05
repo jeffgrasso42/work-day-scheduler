1efa645f5800d2fcbf9ee1c8b0bb361923200e08
JAG <jagrasso42@gmail.com>
2022-10-05 17:31:18 -0400

    added hour display variable to change schedule from 24 hour clock to 12 hour

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,29 +5,43 @@
 // add click event to save activity to local storage
 
 var currentDate = new Date().getHours();
-console.log(currentDate);
 
 for (var i = 9; i < 18; i++) {
   var currentHour = new Date().getHours();
-  var colorKey = '';
+  var colorKey = "";
   if (currentHour > i) {
-    colorKey = 'past';
+    colorKey = "past";
   } else if (currentHour === i) {
-    colorKey = 'present'
+    colorKey = "present";
   } else {
-    colorKey = 'future';
+    colorKey = "future";
   }
-  var row = $('<div>').addClass('row time-block').attr('id', i);
-  var hour = $('<div>').addClass('col-2 hour').text(i);
-  var textArea = $('<textarea>').addClass('col-8 description ' + colorKey).val(localStorage.getItem(i)); // i = hourKey
-  var saveBtn = $('<button>').attr('id', i).addClass('col-2 saveBtn').click(function () {
-    var hourKey = $(this).attr('id');
 
-    var activity = $(this).siblings('.description').val();
-    localStorage.setItem(hourKey, activity);
-  })
-  var icon = $('<i>').addClass('fas fa-save');
+  var hourDisplay = "";
 
-  $('.container').append(
-    row.append(hour, textArea, saveBtn.append(icon)));
+  if (i < 12) {
+    hourDisplay = i + "AM";
+  } else if (i === 12) {
+    hourDisplay = i + "PM";
+  } else {
+    hourDisplay = i - 12 + "PM";
+  }
+
+  var row = $("<div>").addClass("row time-block").attr("id", i);
+  var hour = $("<div>").addClass("col-2 hour").text(hourDisplay);
+  var textArea = $("<textarea>")
+    .addClass("col-8 description " + colorKey)
+    .val(localStorage.getItem(i)); // i = hourKey
+  var saveBtn = $("<button>")
+    .attr("id", i)
+    .addClass("col-2 saveBtn")
+    .click(function () {
+      var hourKey = $(this).attr("id");
+
+      var activity = $(this).siblings(".description").val();
+      localStorage.setItem(hourKey, activity);
+    });
+  var icon = $("<i>").addClass("fas fa-save");
+
+  $(".container").append(row.append(hour, textArea, saveBtn.append(icon)));
 }
